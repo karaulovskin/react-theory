1f16265f8ca4b3db0e551d49ffd42dde5ebe6ada
Karaulovskin <karaulovskin@yandex.ru>
2020-04-03 23:46:00 +0300

    Подключение CSS

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,13 +1,8 @@
 import React from "react";
+import './Track.css'
 
 export default props => (
-    <div style={{
-        marginBottom: '10px',
-        paddingBottom: '20px',
-        border: '1px solid #ccc',
-        boxShadow: '0px 4px 5px 0 rgba(0, 0, 0, .2)',
-        borderRadius: 1
-    }}>
+    <div className="Track">
         <h3>{ props.performer }</h3>
         <p><strong>{ props.name }</strong></p>
         <input type="text" onChange={props.onChangeName} value={props.name} />
