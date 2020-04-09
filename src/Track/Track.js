a52bc900ac18ce4e669d9a06de1004182bed1907
Karaulovskin <karaulovskin@yandex.ru>
2020-04-09 13:29:59 +0300

    Создание Stateful компонента

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -2,29 +2,30 @@ import React from "react";
 import Radium from "radium";
 import classes from './Track.module.scss'
 
-const Track = props => {
-
-    const style = {
-        border: '1px solid #ccc',
-        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .15)',
-        ':hover': {
+class Track extends React.Component {
+    render() {
+        const style = {
             border: '1px solid #ccc',
-            boxShadow: '0 4px 14px 0 rgba(0, 0, 0, .25)',
+            boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .15)',
+            ':hover': {
+                border: '1px solid #ccc',
+                boxShadow: '0 4px 14px 0 rgba(0, 0, 0, .25)',
+            }
         }
-    }
 
-    return (
-        <div className={ classes.Track } style={ style }>
-            <h3>{ props.performer }</h3>
-            <p><strong>{ props.name }</strong></p>
-            <input
-                type="text"
-                onChange={ props.onChangeName }
-                value={ props.name }
-            />
-            <button onClick={ props.onDelete }>Delete</button>
-        </div>
-    )
+        return (
+            <div className={ classes.Track } style={ style }>
+                <h3>{ this.props.performer }</h3>
+                <p><strong>{ this.props.name }</strong></p>
+                <input
+                    type="text"
+                    onChange={ this.props.onChangeName }
+                    value={ this.props.name }
+                />
+                <button onClick={ this.props.onDelete }>Delete</button>
+            </div>
+        )
+    }
 }
 
 export default Radium(Track)
