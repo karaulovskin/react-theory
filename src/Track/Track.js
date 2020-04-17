70b27429bd36f6ab874caec3efdd81a62f033ef1
Karaulovskin <karaulovskin@yandex.ru>
2020-04-17 21:01:58 +0300

    Введение в компоненты высшего порядка

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,58 +1,12 @@
 import React from "react";
-// import Radium from "radium";
 import classes from './Track.module.scss'
+import withClass from "../hoc/withClass";
 
 class Track extends React.Component {
 
-    componentWillReceiveProps(nextProps) {
-        console.log('Track componentWillReceiveProps', nextProps)
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('Track shouldComponentUpdate', nextProps, nextState)
-        return nextProps.name.trim() !== this.props.name.trim()
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-        console.log('Track componentWillUpdate', nextProps, nextState)
-    }
-
-    // static getDerivedStateFromProps(nextProps, prevState) {
-    //     console.log('Track getDerivedStateFromProps', nextProps, prevState)
-    //
-    //     return prevState
-    // }
-
-    componentDidUpdate() {
-        console.log('Track componentDidUpdate')
-    }
-
-    getSnapshotBeforeUpdate() {
-        console.log('Track getSnapshotBeforeUpdate')
-    }
-
-    componentWillUnmount() {
-        console.log('Track componentWillUnmount')
-    }
-
     render() {
-        console.log('Track render')
-
-        // if (Math.random() > 0.7) {
-        //     throw new Error('Track random failed')
-        // }
-
-        const style = {
-            border: '1px solid #ccc',
-            boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .15)',
-            ':hover': {
-                border: '1px solid #ccc',
-                boxShadow: '0 4px 14px 0 rgba(0, 0, 0, .25)',
-            }
-        }
-
         return (
-            <div className={ classes.Track } style={ style }>
+            <React.Fragment>
                 <h3>{ this.props.performer }</h3>
                 <p><strong>{ this.props.name }</strong></p>
                 <input
@@ -61,9 +15,9 @@ class Track extends React.Component {
                     value={ this.props.name }
                 />
                 <button onClick={ this.props.onDelete }>Delete</button>
-            </div>
+            </React.Fragment>
         )
     }
 }
 
-export default Track
+export default withClass(Track, classes.Track)
