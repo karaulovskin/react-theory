048bf05eb73d6802c49106ffe529e3212391fc88
Karaulovskin <karaulovskin@yandex.ru>
2020-05-02 22:13:38 +0300

    Роутинг и компоненты

--- src/App.js
+++ src/App.js
@@ -17,16 +17,18 @@ class App extends Component {
                         <li>
                             <a href="/about">About</a>
                         </li>
+                        <li>
+                            <a href="/cars">Cars</a>
+                        </li>
                     </ul>
                 </nav>
 
                 <hr/>
 
                 <Route path="/" exact render={() => <h1>Home Page</h1>} />
+                <Route path="/about" component={About} />
+                <Route path="/cars" component={Cars} />
 
-                <About />
-
-                <Cars />
             </div>
         );
     }
