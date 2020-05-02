54bd885fd5d55e19cbf1800fe2a187a53de5795e
Karaulovskin <karaulovskin@yandex.ru>
2020-05-02 16:34:25 +0300

    Установка и настройка

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.module.scss';
 import App from './App';
-// import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import serviceWorker from './serviceWorker';
 
 const app = (
-    <App />
-    // <BrowserRouter>
-    //     <App />
-    // </BrowserRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
